f086e0826c6077b7be1f26aeeaa5e3807293d80f
njine <brikab@gmail.com>
2023-12-02 00:06:30 +0300

    sito

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,49 +1,29 @@
 #include "binary_trees.h"
 
 /**
- * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
- * @first: A pointer to the first node.
- * @second: A pointer to the second node.
+ * binary_trees_ancestor - Find the lowest common ancestor of two nodes.
+ * @first: Pointer to the first node.
+ * @second: Pointer to the second node.
  *
- * Return: If no common ancestor was found, return NULL.
- *         Otherwise, return a pointer to the lowest common ancestor node.
+ * Return: If no common ancestors return NULL, else return common ancestor.
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
-    const binary_tree_t *ancestor;
+	binary_tree_t *parent_first, *parent_second;
 
-    if (!first || !second)
-        return (NULL);
+	if (!first || !second)
+		return (NULL);
 
-    ancestor = first;
+	if (first == second)
+		return ((binary_tree_t *)first);
 
-    while (ancestor)
-    {
-        if (binary_tree_is_descendant(ancestor, second))
-            return ((binary_tree_t *)ancestor);
+	parent_first = first->parent, parent_second = second->parent;
 
-        ancestor = ancestor->parent;
-    }
+	if (first == parent_second || !parent_first || (!parent_first->parent && parent_second))
+		return (binary_trees_ancestor(first, parent_second));
+	else if (parent_first == second || !parent_second || (!parent_second->parent && parent_first))
+		return (binary_trees_ancestor(parent_first, second));
 
-    return (NULL);
-}
-
-/**
- * binary_tree_is_descendant - Checks if a node is a descendant of another.
- * @ancestor: The potential ancestor node.
- * @node: The node to check if it is a descendant.
- *
- * Return: If the node is a descendant, return 1. Otherwise, return 0.
- */
-int binary_tree_is_descendant(const binary_tree_t *ancestor, const binary_tree_t *node)
-{
-    while (node)
-    {
-        if (node == ancestor)
-            return (1);
-
-        node = node->parent;
-    }
-
-    return (0);
+	return (binary_trees_ancestor(parent_first, parent_second));
 }
---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,141 @@
+#include "binary_trees.h"
+
+/**
+ * struct levelorder_queue_s - Structure for level-order traversal queue.
+ * @node: A pointer to the binary tree node.
+ * @next: A pointer to the next node in the queue.
+ */
+typedef struct levelorder_queue_s
+{
+    binary_tree_t *node;
+    struct levelorder_queue_s *next;
+} levelorder_queue_t;
+
+levelorder_queue_t *create_node(binary_tree_t *node);
+void free_queue(levelorder_queue_t *head);
+void enqueue(binary_tree_t *node, levelorder_queue_t **tail);
+void dequeue(levelorder_queue_t **head);
+int binary_tree_is_complete(const binary_tree_t *tree);
+
+/**
+ * create_node - Creates a new levelorder_queue_t node.
+ * @node: The binary tree node for the new node to contain.
+ *
+ * Return: If an error occurs, NULL.
+ *         Otherwise, a pointer to the new node.
+ */
+levelorder_queue_t *create_node(binary_tree_t *node)
+{
+    levelorder_queue_t *new;
+
+    new = malloc(sizeof(levelorder_queue_t));
+    if (new == NULL)
+        return (NULL);
+
+    new->node = node;
+    new->next = NULL;
+
+    return (new);
+}
+
+/**
+ * free_queue - Frees a levelorder_queue_t queue.
+ * @head: A pointer to the head of the queue.
+ */
+void free_queue(levelorder_queue_t *head)
+{
+    levelorder_queue_t *tmp;
+
+    while (head != NULL)
+    {
+        tmp = head->next;
+        free(head);
+        head = tmp;
+    }
+}
+
+/**
+ * enqueue - Enqueues a node to the back of a levelorder_queue_t queue.
+ * @node: The binary tree node to enqueue.
+ * @tail: A double pointer to the tail of the queue.
+ *
+ * Description: Upon malloc failure, exits with a status code of 1.
+ */
+void enqueue(binary_tree_t *node, levelorder_queue_t **tail)
+{
+    levelorder_queue_t *new;
+
+    new = create_node(node);
+    if (new == NULL)
+    {
+        free_queue(*tail);
+        exit(1);
+    }
+    (*tail)->next = new;
+    *tail = new;
+}
+
+/**
+ * dequeue - Dequeues the head of a levelorder_queue_t queue.
+ * @head: A double pointer to the head of the queue.
+ */
+void dequeue(levelorder_queue_t **head)
+{
+    levelorder_queue_t *tmp;
+
+    tmp = (*head)->next;
+    free(*head);
+    *head = tmp;
+}
+
+/**
+ * binary_tree_is_complete - Checks if a binary tree is complete.
+ * @tree: A pointer to the root node of the tree to traverse.
+ *
+ * Return: If the tree is NULL or not complete, 0.
+ *         Otherwise, 1.
+ *
+ * Description: Upon malloc failure, exits with a status code of 1.
+ */
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+    levelorder_queue_t *head, *tail;
+    unsigned char flag = 0;
+
+    if (tree == NULL)
+        return (0);
+
+    head = tail = create_node((binary_tree_t *)tree);
+    if (head == NULL)
+        exit(1);
+
+    while (head != NULL)
+    {
+        if (head->node->left != NULL)
+        {
+            if (flag == 1)
+            {
+                free_queue(head);
+                return (0);
+            }
+            enqueue(head->node->left, &tail);
+        }
+        else
+            flag = 1;
+
+        if (head->node->right != NULL)
+        {
+            if (flag == 1)
+            {
+                free_queue(head);
+                return (0);
+            }
+            enqueue(head->node->right, &tail);
+        }
+        else
+            flag = 1;
+
+        dequeue(&head);
+    }
+    return (1);
+}

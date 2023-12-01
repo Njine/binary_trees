9dfb38fa9d11e21b5061d849ef69ce4124e4af10
njine <brikab@gmail.com>
2023-12-01 22:59:15 +0300

    gtimey

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,40 +1,23 @@
 #include "binary_trees.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * binary_tree_postorder - Perform post-order traversal on a binary tree.
- * @tree: Pointer to the root node of the tree to traverse.
- * @func: Pointer to a function to call for each node.
- *
- * Description: This function traverses the binary tree in post-order and
- * applies the given function to each visited node.
+ * binary_tree_postorder - Traverse a binary tree using post-order traversal.
+ * @tree: A pointer to the root node of the tree to traverse.
+ * @func: A pointer to a function to call for each node's value.
+ *        If tree or func is NULL, do nothing.
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
+    /* Check for NULL tree or function pointer */
 	if (tree == NULL || func == NULL)
 		return;
 
-	const binary_tree_t *current = tree;
-	binary_tree_t *stack1[1024]; /* Stack to store nodes */
-	binary_tree_t *stack2[1024]; /* Stack for reverse post-order */
-	size_t stack1_index = 0, stack2_index = 0;
+    /* Traverse left subtree */
+	binary_tree_postorder(tree->left, func);
 
-	stack1[stack1_index++] = current;
+    /* Traverse right subtree */
+	binary_tree_postorder(tree->right, func);
 
-	while (stack1_index > 0)
-	{
-		current = stack1[--stack1_index];
-		stack2[stack2_index++] = current;
-
-		if (current->left)
-			stack1[stack1_index++] = current->left; /* Push left child onto stack1 */
-		if (current->right)
-			stack1[stack1_index++] = current->right; /* Push right child onto stack1 */
-	}
-
-	while (stack2_index > 0)
-	{
-		func(stack2[--stack2_index]->n); /* Process nodes in reverse post-order */
-	}
+    /* Call the function for the current node's value */
+	func(tree->n);
 }

9767a982cd1af1c12fb16ccc0f6aee07214231a1
njine <brikab@gmail.com>
2023-12-01 23:34:40 +0300

    sitwaty

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,27 +1,28 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - Counts the number of nodes with at least one child in a binary tree.
- * @tree: A pointer to the root node of the tree.
+ * binary_tree_nodes - Count nodes with at least 1 child in a binary tree.
+ * @tree: A pointer to the root node of the tree to count the number of nodes.
  *
- * Return: Number of nodes with at least one child. If @tree is NULL, the function returns 0.
+ * Return: If tree is NULL, the function must return 0.
+ *         Else return the count of nodes.
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    /* If tree is NULL, return 0 (empty tree) */
-    if (!tree)
-        return (0);
+	size_t count = 0;
 
-    /* Initialize the count of nodes with at least one child */
-    size_t node_count = 0;
+	/* Check if tree is NULL */
+	if (tree)
+	{
+		/* Increment count if the node has at least 1 child */
+		count += (tree->left || tree->right) ? 1 : 0;
 
-    /* Check if the current node has at least one child and increment the count accordingly */
-    node_count += ((tree->left || tree->right) ? 1 : 0);
+		/* Recursively count nodes in the left subtree */
+		count += binary_tree_nodes(tree->left);
 
-    /* Recursively count nodes with at least one child in the left and right subtrees */
-    node_count += binary_tree_nodes(tree->left);
-    node_count += binary_tree_nodes(tree->right);
+		/* Recursively count nodes in the right subtree */
+		count += binary_tree_nodes(tree->right);
+	}
 
-    /* Return the total count of nodes with at least one child */
-    return (node_count);
+	return (count);
 }

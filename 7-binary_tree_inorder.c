411a565c0e09945cd4829fcffdc56b1634331b4d
njine <brikab@gmail.com>
2023-12-01 22:39:15 +0300

    gtime

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,37 +1,23 @@
 #include "binary_trees.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * binary_tree_inorder - Perform in-order traversal on a binary tree.
- * @tree: Pointer to the root node of the tree to traverse.
- * @func: Pointer to a function to call for each node.
- *
- * Description: This function traverses the binary tree in in-order and
- * applies the given function to each visited node.
+ * binary_tree_inorder - Traverse a binary tree using in-order traversal.
+ * @tree: A pointer to the root node of the tree to traverse.
+ * @func: A pointer to a function to call for each node's value.
+ *        If tree or func is NULL, do nothing.
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
+    /* Check for NULL tree or function pointer */
 	if (tree == NULL || func == NULL)
 		return;
 
-	const binary_tree_t *current = tree;
-	binary_tree_t *stack[1024]; /* Assuming a maximum stack size of 1024 */
-	size_t stack_index = 0;
+    /* Traverse left subtree */
+	binary_tree_inorder(tree->left, func);
 
-	while (current || stack_index > 0)
-	{
-		while (current)
-		{
-			stack[stack_index++] = current; /* Push the current node onto the stack */
-			current = current->left; /* Move to the left child */
-		}
+    /* Call the function for the current node's value */
+	func(tree->n);
 
-		if (stack_index > 0)
-		{
-			current = stack[--stack_index]; /* Pop a node from the stack */
-			func(current->n); /* Process the current node */
-			current = current->right; /* Move to the right child */
-		}
-	}
+    /* Traverse right subtree */
+	binary_tree_inorder(tree->right, func);
 }
